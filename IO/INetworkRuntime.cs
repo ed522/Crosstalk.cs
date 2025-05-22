042bc88ba8b610c17d22f95689c7267970c186fb
ed522 <147557022+ed522@users.noreply.github.com>
2025-05-22 17:09:16 -0400

    start network framework

--- IO/INetworkRuntime.cs
+++ IO/INetworkRuntime.cs
@@ -1,7 +1,13 @@
+using System.Numerics;
+
 namespace Crosstalk.IO;
+
+record SocketData(string Address, ushort Port);
 
 interface INetworkRuntime {
 
-    public void Initialize()
+    public void Initialize(Span<byte> dataBuffer);
+    public void Serialize(object data);
+    public T Deserialize<T>();
 
 }
